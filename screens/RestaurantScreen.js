ca748a5ccd6acf72f6fb31c04d84a2e809f40ea3
Oscar Espana <espaaoscar@gmail.com>
2022-08-02 09:37:31 -0500

    feat: building the Dish Rows section

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -15,6 +15,7 @@ import {
   LocationMarkerIcon,
   StarIcon,
 } from 'react-native-heroicons/solid';
+import DishRow from '../components/DishRow';
 import {urlFor} from '../sanity';
 const RestaurantScreen = () => {
   const navigation = useNavigation();
@@ -81,6 +82,20 @@ const RestaurantScreen = () => {
           </Text>
           <ChevronRightIcon color="#00CCBB" />
         </TouchableOpacity>
+      </View>
+      <View>
+        <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+        {/* Dishrows */}
+        {dishes.map(dish => (
+          <DishRow
+            key={dish._id}
+            id={dish._id}
+            name={dish.name}
+            description={dish.description}
+            price={dish.price}
+            image={dish.image}
+          />
+        ))}
       </View>
     </SafeAreaView>
   );
