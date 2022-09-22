4a046aa4b648faeced6328e15862197917ab53b6
Oscar Espana <espaaoscar@gmail.com>
2022-09-21 20:10:40 -0500

    style: wrapping with ScrollView the Restaurant screen

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -57,46 +57,49 @@ const RestaurantScreen = () => {
             <ArrowLeftIcon size={20} color="#00CCBB" />
           </TouchableOpacity>
         </View>
-      </ScrollView>
-      <View className="bg-white">
-        <View className="px-4 pt-4">
-          <Text className="text-3xl font-bold">{title}</Text>
-          <View className="flex-row space-x-2 my-1">
-            <View className="flex-row items-center space-x-1">
-              <StarIcon color="green" opacity={0.5} size={22} />
-              <Text className="text-xs text-gray-500">
-                <Text className="text-green-500">{rating}</Text> · {genre}
-              </Text>
+
+        <View className="bg-white">
+          <View className="px-4 pt-4">
+            <Text className="text-3xl font-bold">{title}</Text>
+            <View className="flex-row space-x-2 my-1">
+              <View className="flex-row items-center space-x-1">
+                <StarIcon color="green" opacity={0.5} size={22} />
+                <Text className="text-xs text-gray-500">
+                  <Text className="text-green-500">{rating}</Text> · {genre}
+                </Text>
+              </View>
+              <View className="flex-row items-center space-x-1">
+                <LocationMarkerIcon color="gray" opacity={0.4} size={22} />
+                <Text className="text-xs text-gray-500">
+                  Nearby · {address}
+                </Text>
+              </View>
             </View>
-            <View className="flex-row items-center space-x-1">
-              <LocationMarkerIcon color="gray" opacity={0.4} size={22} />
-              <Text className="text-xs text-gray-500">Nearby · {address}</Text>
-            </View>
+            <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
           </View>
-          <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
+          <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
+            <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20} />
+            <Text className="pl-2 flex-1 text-base font-bold">
+              Have a food allergy?
+            </Text>
+            <ChevronRightIcon color="#00CCBB" />
+          </TouchableOpacity>
         </View>
-        <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
-          <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20} />
-          <Text className="pl-2 flex-1 text-base font-bold">
-            Have a food allergy?
-          </Text>
-          <ChevronRightIcon color="#00CCBB" />
-        </TouchableOpacity>
-      </View>
-      <View>
-        <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
-        {/* Dishrows */}
-        {dishes.map(dish => (
-          <DishRow
-            key={dish._id}
-            id={dish._id}
-            name={dish.name}
-            description={dish.description}
-            price={dish.price}
-            image={dish.image}
-          />
-        ))}
-      </View>
+        <View>
+          <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+          {/* Dishrows */}
+          {dishes.map(dish => (
+            <DishRow
+              key={dish._id}
+              id={dish._id}
+              name={dish.name}
+              description={dish.description}
+              price={dish.price}
+              image={dish.image}
+            />
+          ))}
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
