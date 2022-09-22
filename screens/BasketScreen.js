879feebe84bf8a7f4851cff4b1128afd6439aa85
Oscar Espana <espaaoscar@gmail.com>
2022-09-21 22:07:00 -0500

    feat: implementing grouping menu items funtionality

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,5 +1,5 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {useDispatch, useSelector} from 'react-redux';
 import {selectRestaurant} from '../features/restauranteSlice';
@@ -10,6 +10,16 @@ const BasketScreen = () => {
   const navigation = useNavigation();
   const restaurant = useSelector(selectRestaurant);
   const items = useSelector(selectBasketItems);
+
+  const [groupedItemsInBasket, setGroupedItemsInBasket] = useState([]);
+
+  useEffect(() => {
+    const groupedItems = items.reduce((results, item) => {
+      (results[item.id] = results[items.id] || []).push(item);
+      return results;
+    }, {});
+    setGroupedItemsInBasket(groupedItems);
+  }, [items]);
 
   return (
     <View>
